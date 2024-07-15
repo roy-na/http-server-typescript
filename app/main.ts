32cbf491b7a3cb2186732669b89154548b406356
roy-na <royna@wix.com>
2024-07-15 14:02:37 +0300

    pass 4st stage

--- app/main.ts
+++ app/main.ts
@@ -16,8 +16,7 @@ const server = net.createServer((socket) => {
         const request = data.toString().split(' ')[1]
         console.log(request)
         if(request.startsWith(ROUTES.ECHO))  {
-            const content = request.split(ROUTES.ECHO)
-            console.log(content, content.length)
+            const content = request.replace(ROUTES.ECHO, '')
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\ ${content}`)
         } else {
             socket.write(HTML_STATUS.NOT_FOUND)
