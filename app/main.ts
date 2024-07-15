8a543845c2e1e4bc6eed4ab007f297812ccb70d3
roy-na <royna@wix.com>
2024-07-15 13:53:14 +0300

    pass 3st stage

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,5 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-const createBuffer = (string: string) => {
-    return Buffer.from(string)
-}
-
 const HTML_STATUS = {
     OK: `HTTP/1.1 200 OK\r\n\r\n`,
     NOT_FOUND: `HTTP/1.1 404 Not Found\r\n\r\n`
@@ -14,15 +7,15 @@ const HTML_STATUS = {
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.on("data", data => {
+    socket.on("data", (data) => {
         const request = data.toString().split(' ')
         if(request[1] === '/') {
             socket.write(HTML_STATUS.OK)
         } else {
             socket.write(HTML_STATUS.NOT_FOUND)
         }
+        socket.end();
     })
-    socket.end();
 });
 
 server.listen(4221, "localhost");
