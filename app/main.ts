9bacb804de5fcab98bda984b63ef587ce5dfc40d
roy-na <royna@wix.com>
2024-07-17 17:01:15 +0300

    Read Header

--- app/main.ts
+++ app/main.ts
@@ -3,14 +3,14 @@ import { HTML_STATUS, ROUTES } from "./consts";
 
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        const request = getRequestRoute(data)
+        const request = data.toString();
+        console.log(request);
+        const path = request.split(' ')[1];
+        console.log(path.split('/')[1])
+        const params = path.split('/')[1];
 
-        switch (request) {
+        switch (params) {
 
-            case ROUTES.ECHO: {
-                const content = request.replace(ROUTES.ECHO, '')
-                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
-            }
             case ROUTES.ECHO: {
                 const content = request.replace(ROUTES.ECHO, '')
                 socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
@@ -20,6 +20,7 @@ const server = net.createServer((socket) => {
                 const userAgent = data.toString().split('User-Agent: ')[1].split('\r\n')[0]
                 socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
             }
+            
             default: {
                 if (request === ROUTES.ROOT) {
                     socket.write(HTML_STATUS.OK)
