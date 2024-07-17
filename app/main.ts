6efc5f871076b7f385002a55d169109148246b9f
roy-na <royna@wix.com>
2024-07-17 17:34:09 +0300

    File system

--- app/main.ts
+++ app/main.ts
@@ -30,7 +30,7 @@ const server = net.createServer((socket) => {
             }
             
             case ROUTES.FILES: {
-                const content = fs.readFileSync('./tmp' + params);
+                const content = fs.readFileSync('./tmp/' + params.);
                 socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
                 socket.end();
                 break
