73c20555e4d4f8354995a3e796b9d33cd61281f9
roy-na <royna@wix.com>
2024-07-21 21:01:48 +0300

    Add encoding option

--- app/main.ts
+++ app/main.ts
@@ -2,17 +2,25 @@ import * as net from "net";
 import fs from "node:fs";
 import * as process from 'process';
 import { HTML_STATUS, ROUTES } from "./consts";
+import { arrayToObject } from "./utils";
 
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        const request = data.toString();            // Full request 
-        const [method, path] = request.split(" ");  // Request path
-        const params = path.split('/')[1];          // Params sent after the path
-        const requestContent = path.split('/')[2]   // Content of some request
-        const [, ...headers] = data.toString().split("\r\n");
-        const [body] = headers.splice(headers.length - 1);
+        const request = data.toString();  
+        const [requestLine, ...restOfheaders] = data.toString().split("\r\n");
+        const [body] = restOfheaders.splice(restOfheaders.length - 1);
+        const [method, path] = requestLine.split(" ");
+        const [root, pathRoute, content, ...restParameters] = path.split('/');
+        console.log("requestLine: ", requestLine)
+        const headers = arrayToObject(restOfheaders)
+        console.log("body: ", body)
+        console.log("method: ", method)
+        console.log("path: ", path)
+        console.log("root: ", root)
+        console.log("restParameters: ", restParameters)
+        console.log("content: ", content)
 
-        switch (params) {
+        switch (pathRoute) {
 
             case ROUTES.ROOT: {
                 socket.write(HTML_STATUS.OK)
@@ -20,7 +28,11 @@ const server = net.createServer((socket) => {
                 break
             }
             case ROUTES.ECHO: {
-                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${requestContent.length}\r\n\r\n${requestContent}`)
+                if(headers["Accept-Encoding"] === "gzip"){
+                    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
+                    socket.end();
+                }
+                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
                 socket.end();
                 break
             }
@@ -36,11 +48,11 @@ const server = net.createServer((socket) => {
                 const directory: string = process.argv[3];
                 try {
                     if(method === 'GET') {
-                        const content = fs.readFileSync(directory + requestContent);
+                        const fileContent = fs.readFileSync(directory + content);
                         socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
                         socket.end();
                     } else {
-                        fs.writeFileSync(directory + requestContent, body);
+                        fs.writeFileSync(directory + content, body);
                         socket.write(HTML_STATUS.CREATED)
                         socket.end();
 
--- app/utils.ts
+++ app/utils.ts
@@ -4,3 +4,13 @@ function getRequestRoute(request: Buffer) {
 function getFullRequestRoute(request: Buffer) {
     return request.toString().split(' ')[1]
 }
+
+export function arrayToObject(array: string[]): { [key: string]: string } {
+    return array.reduce((acc, item) => {
+        if (item.trim() !== "") {
+            const [key, ...rest] = item.split(':');
+            acc[key.trim()] = rest.join(':').trim();
+        }
+        return acc;
+    }, {} as { [key: string]: string });
+}
