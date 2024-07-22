fa3e180681d62abe8c0879e581c5bd744defa0bc
roy-na <royna@wix.com>
2024-07-22 12:07:50 +0300

    refactore

---
+++ app/HttpServerRequest.ts
@@ -0,0 +1,109 @@
+import fs from "node:fs";
+import * as process from 'process';
+import { HTML_STATUS, ROUTES } from "./consts";
+import { gzipSync } from "zlib";
+import { arrayToObject } from "./utils"
+
+export class HttpServerRequest {
+    private headers: Record<string, string>
+    private body: string
+    private method: string
+    private pathRoute: string
+    private content: string
+    private encoding: string
+    private response: string
+
+    constructor(request: string) {
+        const [requestLine, ...restOfheaders] = request.split("\r\n");
+        const [body] = restOfheaders.splice(restOfheaders.length - 1);
+        const [method, path] = requestLine.split(" ");
+        const [root, pathRoute, content, ...restParameters] = path.split('/');
+
+        this.headers = arrayToObject(restOfheaders)
+        this.body = body
+        this.method = method
+        this.pathRoute = pathRoute
+        this.content = content
+        this.encoding = this.headers['Accept-Encoding'].includes('gzip') ? 'gzip' : ''
+        this.response = `HTTP/1.1 200 OK\r\n`
+
+    }
+
+    responseBuilder = {
+        withContentType: (type: string) => {
+            this.response + `Content-Type: ${type}\r\n`
+            return this.responseBuilder
+        },
+        withContentLength: (length: number) => {
+            this.response + `Content-Length: ${length}\r\n\r\n`
+            return this.responseBuilder
+        },
+        withContent: (content: Buffer | string) => {
+            this.response + content
+            return this.responseBuilder
+        },
+        flush: () => {
+            const responseMessage = this.response
+            this.response = `HTTP/1.1 200 OK\r\n`
+            return responseMessage
+        }
+    }
+
+    // Read and write from or to file system
+    private read(path: string) {
+        const fileContent = fs.readFileSync(path);
+        const response = this.responseBuilder
+            .withContentType('text/plain')
+            .withContentLength(fileContent.length)
+            .withContent(fileContent)
+            .flush()
+        return response
+    }
+
+    private write(path: string, content: string) {
+        fs.writeFileSync(path, content);
+        return HTML_STATUS.CREATED
+    }
+
+    handleFileSystem() {
+        const directory: string = process.argv[3];
+        const path = directory + this.content
+        try {
+            if (this.method === 'GET') {
+                return this.read(path)
+            } else if (this.method === 'POST') {
+                return this.write(path, this.body)
+            }
+        } catch (e) {
+            console.error(e)
+        }
+        return HTML_STATUS.NOT_FOUND
+    }
+
+    getUserAgent() {
+        const userAgent = this.headers['User-Agent']
+        const response = this.responseBuilder
+            .withContentType('text/plain')
+            .withContentLength(userAgent.length)
+            .withContent(userAgent)
+            .flush()
+        return response
+    }
+
+    echo() {
+        this.responseBuilder.withContentType('text/plain')
+        let response
+        if (this.encoding === 'gzip') {
+            const buffer = Buffer.from(this.content, 'utf8');
+            const encodedBody = gzipSync(buffer)
+            response = this.responseBuilder.withContentLength(encodedBody.length).flush()
+            return { response, encodedBody }
+        }
+        response = this.responseBuilder.withContentLength(this.content.length).withContent(this.content).flush()
+        return { response, encodedBody: '' }
+    }
+
+    getRoutPath() {
+        return this.pathRoute
+    }
+}
--- app/main.ts
+++ app/main.ts
@@ -4,17 +4,13 @@ import * as process from 'process';
 import { HTML_STATUS, ROUTES } from "./consts";
 import { arrayToObject } from "./utils";
 import { gzipSync } from "zlib";
+import { HttpServerRequest } from "./HttpServerRequest";
+
 
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        const request = data.toString();  
-        const [requestLine, ...restOfheaders] = data.toString().split("\r\n");
-        const [body] = restOfheaders.splice(restOfheaders.length - 1);
-        const [method, path] = requestLine.split(" ");
-        const [root, pathRoute, content, ...restParameters] = path.split('/');
-        const headers = arrayToObject(restOfheaders)
-
-        switch (pathRoute) {
+        const httpServerRequest = new HttpServerRequest(data.toString())
+        switch (httpServerRequest.getRoutPath()) {
 
             case ROUTES.ROOT: {
                 socket.write(HTML_STATUS.OK)
@@ -22,42 +18,23 @@ const server = net.createServer((socket) => {
                 break
             }
             case ROUTES.ECHO: {
-                if(headers["Accept-Encoding"]?.includes("gzip")){
-                    const buffer = Buffer.from(content, 'utf8');
-                    const encodedBody = gzipSync(buffer)
-                    socket.write((`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: ${encodedBody.length}\r\n\r\n`))
-                    socket.write(encodedBody)
-                    socket.end();
-                }
-                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
+               const { response, encodedBody } = httpServerRequest.echo()
+                socket.write(response)
+                encodedBody.length && socket.write(encodedBody) 
                 socket.end();
                 break
             }
 
             case ROUTES.USER_AGENT: {
-                const userAgent = request.split('User-Agent: ')[1].split('\r\n')[0]
-                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
+                socket.write(httpServerRequest.getUserAgent())
                 socket.end();
                 break
             }
 
             case ROUTES.FILES: {
-                const directory: string = process.argv[3];
-                try {
-                    if(method === 'GET') {
-                        const fileContent = fs.readFileSync(directory + content);
-                        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`)
-                        socket.end();
-                    } else {
-                        fs.writeFileSync(directory + content, body);
-                        socket.write(HTML_STATUS.CREATED)
-                        socket.end();
 
-                    }
-                } catch (e) {
-                    socket.write(HTML_STATUS.NOT_FOUND)
-                    socket.end();
-                }
+                socket.write(httpServerRequest.handleFileSystem())
+                socket.end();
                 break
             }
             default: {
